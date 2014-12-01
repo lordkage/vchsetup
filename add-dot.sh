a7ca5d9543b692ab83c8ce32ea830fced02369b6
Kage Lyall <kage@temporalflux.org>
2014-12-01 15:26:32 -0800

    Updates to add-dot.sh
     - Adding repo name presence checking
     - Adding remote to vcsh
     - Adding push origin to vcsh
     - Consolidating mr bits

--- add-dot.sh
+++ add-dot.sh
@@ -3,6 +3,7 @@
 _start_dir=`pwd` ; cd ${HOME}
 
 _config_name=$1
+if [[ ${_config_name} == '' ]]; then echo "BORK; please provide a repo name"; exit 128; fi
 _config_repo="gitosis@gitosis.temporalflux.org:dot/${_config_name}.git" # Replace with the right remote repo
 
 # Create the remote repository named: dot/${_config_name}
@@ -16,6 +17,16 @@ until [[ -z "$_dot_file" ]]; do
       vcsh ${_config_name} add ${_dot_file}
    fi
 done
+
+# Generate the gitignore file for the configuration repository
+touch ~/.gitignore.d/${_config_name}
+vcsh ${_config_name} add -f ~/.gitignore.d/${_config_name}
+vcsh write-gitignore ${_config_name}
+
+# Commit vcsh
+vcsh ${_config_name} commit -a -m "Initial commit of vcsh ${_config_name}"
+vcsh ${_config_name} remote add origin ${_config_repo}
+vcsh ${_config_name} push -u origin master
 
 # Confirm mr is up-to-date
 vcsh mr pull
@@ -31,15 +42,6 @@ vcsh mr add -f ~/.config/mr/available.d/vcsh.${_config_name}
 # Add to config.d
 _cur_dir=`pwd`; cd ~/.config/mr/config.d; ln -s ../available.d/vcsh.${_config_name} ; cd ${_cur_dir}
 vcsh mr add -f ~/.config/mr/config.d/vcsh.${_config_name}
-
-# Generate the gitignore file for the configuration repository
-touch ~/.gitignore.d/${_config_name}
-vcsh ${_config_name} add -f ~/.gitignore.d/${_config_name}
-vcsh write-gitignore ${_config_name}
-
-# Commit vcsh
-vcsh ${_config_name} commit -a -m "Initial commit of vcsh ${_config_name}"
-vcsh ${_config_name} push
 
 # Commit (and push) changes
 vcsh mr commit -m "Added vcsh ${_config_name} and link"

63b695e8b275fddebf85276dcf91c88fb198a7fe
Kage Lyall <kage@temporalflux.org>
2014-12-09 16:40:04 -0800

    Fixes to mrQuickAdd.sh
     - Fixed sed to not produce backup
     - IMP'd prefix
     - Fixed a minor berp

--- mrQuickAdd.sh
+++ mrQuickAdd.sh
@@ -4,10 +4,15 @@
 
 _working_dir=$PWD
 _working_project_name=${PWD##*/}
+_working_project_config_name=${_working_project_name}
 _working_project_url=$(git config --get remote.origin.url)
 _mr_configuration_dir="${HOME}/.config/mr"
 _mr_available_dir="${_mr_configuration_dir}/available.d"
-_mr_project_config="${_mr_available_dir}/${_working_project_name}"
+if [[ $# -gt 0 ]]; then
+   _mr_project_config_prefix=$1; shift
+   _working_project_config_name="${_mr_project_config_prefix}-${_working_project_config_name}"
+fi
+_mr_project_config="${_mr_available_dir}/${_working_project_config_name}"
 _mr_configured_dir="${_mr_configuration_dir}/config.d"
 
 if [ ! -d ${_working_dir}/.git ]; then
@@ -16,7 +21,7 @@ if [ ! -d ${_working_dir}/.git ]; then
 fi
 
 mr -c ${_mr_project_config} register
-sed -pi -e "s#${HOME}#\${HOME}#" ${_mr_project_config}
+sed -i '' -e "s#${HOME}#\${HOME}#" ${_mr_project_config}
 
 cat >>${_mr_project_config}<<_EOF_
 update = git remote prune origin && git pull
@@ -25,7 +30,7 @@ commit = :
 _EOF_
 
 cd ${_mr_configured_dir}
-ln -vs ../available.d/${_working_project_name}
+ln -vs ../available.d/${_working_project_config_name}
 cd ${_working_dir}
 
 vcsh mr status --short --branch -unormal

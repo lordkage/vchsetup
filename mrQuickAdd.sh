bed461b5d1f964134f85e96cd2a8ba66da709d97
Kage Lyall <kage@temporalflux.org>
2015-06-23 08:45:59 -0700

    mrQuickAdd.sh improvements
     - Add check for second  param, if present assume git URI and clone
     - Check if project config file exist before attempting to create one
     - Only specifically add and commit available.d file
     - Create and leave config.d file unadded/uncommitted
     - Changed update line to be a bit more inclusive

--- mrQuickAdd.sh
+++ mrQuickAdd.sh
@@ -2,6 +2,21 @@
 
 # FIXME - Add pre-fix
 
+if [[ $# -eq 2 ]]; then
+   _git_repo_uri=$2
+   IFS='./' read -ra _git_repo_uri_parts <<< "${_git_repo_uri}"
+   _git_repo_dir=${_git_repo_uri_parts[2]}
+   if [ -d $PWD/${_git_repo_dir}/.git ]; then
+      echo "Repository directory ${_git_repo_dir} already exists, assuming correct."
+   elif [ -d $PWD/${_git_repo_dir} ]; then
+      echo "Directory named ${_git_repo_dir} already exists, BORK."
+      exit 1
+   else
+      git clone ${_git_repo_uri} ${_git_repo_dir}
+   fi
+   cd ${_git_repo_dir}
+fi
+
 _working_dir=$PWD
 _working_project_name=${PWD##*/}
 _working_project_config_name=${_working_project_name}
@@ -20,11 +35,16 @@ if [ ! -d ${_working_dir}/.git ]; then
    exit
 fi
 
+if [ -e ${_mr_project_config} ]; then
+   echo "Project configuration (${_mr_project_config}) pre-exists; aborting"
+   exit 1
+fi
+
 mr -c ${_mr_project_config} register
 sed -i '' -e "s#${HOME}#\${HOME}#" ${_mr_project_config}
 
 cat >>${_mr_project_config}<<_EOF_
-update = git remote prune origin && git pull
+update = git remote prune origin && git fetch --all && git fetch --tags && git pull
 push = :
 commit = :
 _EOF_
@@ -39,6 +59,6 @@ cat >/dev/stdout<<_EOF_
 Remember to add the new config to mr's repo
 
 vcsh mr add ${_mr_project_config}
-vcsh mr commit -a -m "Adding ${_working_project_name}"
+vcsh mr commit -m "Adding ${_working_project_name}"
 vcsh mr push
 _EOF_

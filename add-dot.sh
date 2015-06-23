2c2faf63ce9aeff2b7ae167babce7dc904c8ecaf
Kage Lyall <kage@temporalflux.org>
2015-06-23 08:50:40 -0700

    add-dot.sh updates
     - Added useful diagnostic messages
     - Verifies existence of vcsh controlled repo, init's if necessary
     - Sets up the frame work for encrypted dot files
     - Fixes the git ignore create/add order
     - Updates final echo of commands to run

--- add-dot.sh
+++ add-dot.sh
@@ -20,10 +20,20 @@ _mr_repo_file="vcsh.${_config_name}"
 _mr_repo_config="${_mr_available_dir}/${_mr_repo_file}"
 _mr_configured_dir="${_mr_configuration_dir}/config.d"
 
-# Create the remote repository named: dot/${_config_name}
-vcsh init ${_config_name}
+echo "Using ${HOME} as base for all actions within script\n"
 
-#  Add files
+_num_commits=$(vcsh ${_config_name} log --oneline | grep -c .)
+xc=${PIPESTATUS[0]}
+if [ ${xc} -ne 0 ]; then
+   # Create the remote repository named: dot/${_config_name}
+   vcsh init ${_config_name}
+   _num_commits=0
+   _commit_msg="Initial commit of vcsh ${_config_name}"
+else
+   _commit_msg="Assisted commit of vcsh ${_config_name}"
+fi
+
+# Add files
 echo "Enter the dot files; new line to end"
 read -p '>>> ' -e _dot_file
 until [[ -z "$_dot_file" ]]; do
@@ -31,14 +41,34 @@ until [[ -z "$_dot_file" ]]; do
    read -p '>>> ' -e _dot_file
 done
 
+_user_resp='n'
+read -p "Any dot files needing encryption? " -e _user_resp
+if [[ ${_user_resp} =~ ^[Yy]* ]]; then
+   echo "All your encrypted dots are belong to no one; this is an unimplemented function"
+   echo "FIXME : touch ${HOME}/.gitattributes.d/${_config_name}"
+   # Add encrypted files
+   echo "Enter the dot files you wish to be encrypted; new line to end"
+   read -p '>>> ' -e _dot_file
+   until [[ -z "$_dot_file" ]]; do
+      echo "FIXME : Need to add the magic : ${_dot_file}"
+#      vcsh ${_config_name} add ${_dot_file}
+      read -p '>>> ' -e _dot_file
+   done
+   echo "FIXME : vcsh ${_config_name} add ${HOME}/.gitattributes.d/${_config_name}"
+fi
+
 # Generate the gitignore file for the configuration repository
-touch ~/.gitignore.d/${_config_name}
-vcsh ${_config_name} add ~/.gitignore.d/${_config_name}
+touch ${HOME}/.gitignore.d/${_config_name}
+vcsh ${_config_name} add ${HOME}/.gitignore.d/${_config_name}
 vcsh write-gitignore ${_config_name}
 
+# FIXME - Add check for branch.master.rebase
+cat >/dev/stdout<<_EOF_
 # Commit vcsh
-vcsh ${_config_name} commit -a -m "Initial commit of vcsh ${_config_name}"
+vcsh ${_config_name} config branch.master.rebase true
+vcsh ${_config_name} commit -a -m "${_commit_msg}"
 vcsh ${_config_name} remote add origin ${_config_repo}
 vcsh ${_config_name} push -u origin master
+_EOF_
 
 cd ${_start_dir}

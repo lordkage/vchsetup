0dc41edcd31b0dddce997b3f7ecfce8f3d285072
Kage Lyall <kage@temporalflux.org>
2017-01-10 22:51:42 -0800

    Adjusting for actual implementation of git-crypt

--- add-dot.sh
+++ add-dot.sh
@@ -2,17 +2,25 @@
 
 _start_dir=$PWD ; cd ${HOME}
 
+export VCSH_GITATTRIBUTES=none
+
+_is_git_crypt='n'
+
 if [[ $# -ne 1 ]]; then
    echo "BORK; please provide a repo name"
    exit 128
 elif [[ $# -eq 1 ]]; then
+   _config_repo_prefix='dot'
    _config_name=$1
+elif [[ $# -eq 2 ]]; then
+   _config_repo_prefix=$1
+   _config_name=$2
 else
    echo "BORK; too many args"
    exit 1
 fi
 
-_config_repo="gitosis@gitosis.temporalflux.org:dot/${_config_name}.git" # Replace with the right remote repo
+_config_repo="gitosis@gitosis.temporalflux.org:${_config_repo_prefix}/${_config_name}.git" # Replace with the right remote repo
 
 _mr_configuration_dir=".config/mr"
 _mr_available_dir="${_mr_configuration_dir}/available.d"
@@ -42,18 +50,23 @@ done
 
 _user_resp='n'
 read -p "Any dot files needing encryption? " -e _user_resp
-if [[ ${_user_resp} =~ ^[Yy]* ]]; then
-   echo "All your encrypted dots are belong to no one; this is an unimplemented function"
-   echo "FIXME : touch ${HOME}/.gitattributes.d/${_config_name}"
+if [[ ${_user_resp} == [Yy]* ]]; then
+   echo "All your encrypted dots are belong to us; this is an experimental function"
+   touch ${HOME}/.gitattributes.d/${_config_name}
+   vcsh upgrade ${_config_name}
+   vcsh ${_config_name} crypt init
    # Add encrypted files
    echo "Enter the dot files you wish to be encrypted; new line to end"
    read -p '>>> ' -e _dot_file
    until [[ -z "$_dot_file" ]]; do
-      echo "FIXME : Need to add the magic : ${_dot_file}"
-#      vcsh ${_config_name} add ${_dot_file}
+      echo "${_dot_file} filter=git-crypt diff=git-crypt" >> ${HOME}/.gitattributes.d/${_config_name}
+      vcsh ${_config_name} add ${_dot_file}
       read -p '>>> ' -e _dot_file
    done
-   echo "FIXME : vcsh ${_config_name} add ${HOME}/.gitattributes.d/${_config_name}"
+   vcsh ${_config_name} add ${HOME}/.gitattributes.d/${_config_name}
+   _is_git_crypt='y'
+   echo ''; echo "Output key; new line to end"
+   read -p '>>> ' -e _key_path
 fi
 
 # Generate the gitignore file for the configuration repository
@@ -63,11 +76,19 @@ vcsh write-gitignore ${_config_name}
 
 # FIXME - Add check for branch.master.rebase
 cat >/dev/stdout<<_EOF_
+
 # Commit vcsh
 vcsh ${_config_name} config branch.master.rebase true
 vcsh ${_config_name} commit -a -m "${_commit_msg}"
 vcsh ${_config_name} remote add origin ${_config_repo}
 vcsh ${_config_name} push -u origin master
 _EOF_
+
+if [[ $_is_git_crypt == y ]]; then
+   cat >/dev/stdout<<_EOF_
+vcsh ${_config_name} crypt export-key ${_key_path}
+vcsh ${_config_name} crypt status
+_EOF_
+fi
 
 cd ${_start_dir}
